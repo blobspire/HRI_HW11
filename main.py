0f900f235bb3ae6e3a90959f6d66f966ac9b81b2
Cole Wendrowski <blobspire@gmail.com>
2026-04-22 15:26:56 -0400

    Avoid collisions

--- main.py
+++ main.py
@@ -27,8 +27,13 @@ def human_cost(human_actions, initial_human_state, initial_robot_state, robot_ac
         human_state = x[0]
         robot_state = x[1]
         # design your state cost (for the human) here!
-        cost += 1/np.linalg.norm(human_state - robot_state)
-        cost += -human_state[0]
+        cost += 1/np.linalg.norm(human_state - robot_state) # Stay away from the robot (cost bigger as closer)
+        # cost += -human_state[0] # Maximize distance along first (x) axis (left to right)
+
+        # If collide, add big cost
+        if np.linalg.norm(human_state - robot_state) < 0.01:
+            cost += 1000
+            break
     return cost
 
 # robot cost function
@@ -41,6 +46,11 @@ def robot_cost(robot_actions, initial_human_state, initial_robot_state, human_ac
         robot_state = x[1]
         # design your state cost (for the robot) here!
         cost += -human_state[0]
+
+        # If collide, add big cost
+        if np.linalg.norm(human_state - robot_state) < 0.01:
+            cost += 1000
+            break
     return cost
 
 # plot the trajectories
